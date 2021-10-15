c399d5e6c7795ba0719fb60f604740b4211b3429
Daniel Thalmann <daniel@thalmann.li>
2021-10-15 21:34:26 +0200

    libft

--- libft/src/ft_isascii.c
+++ libft/src/ft_isascii.c
@@ -12,6 +12,13 @@
 
 #include "libft.h"
 
+/**
+ * @brief checks whether c is a 7-bit unsigned char value that fits 
+ * 		  into the ASCII character set.
+ * 
+ * @param c character
+ * @return int return non-zero value if is a alphabetic character
+ */
 int	ft_isascii(int c)
 {
 	return (c > -1 && c < 128);
