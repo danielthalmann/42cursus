dfc52aaa5259a36200aeeb8a62986a829aff5b3f
Daniel Thalmann <daniel@thalmann.li>
2021-11-12 15:23:15 -0800

    so_long

--- so_long/src/sprite.c
+++ so_long/src/sprite.c
@@ -6,7 +6,7 @@
 /*   By: dthalman <daniel@thalmann.li>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 06:39:26 by dthalman          #+#    #+#             */
-/*   Updated: 2021/11/12 14:57:05 by dthalman         ###   ########.fr       */
+/*   Updated: 2021/11/12 15:22:19 by dthalman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ void	ft_load_image(char *filename)
 	img_ptr = mlx_png_file_to_image(game->gl.mlx_ptr, filename, (int *)&(size.width), (int *)&(size.height));
 
 	
-	char *point = 0;
+	t_uint *point = 0;
 	int bits_per_pixel = 0;
 	int size_line = 0;
 	int endian = 0;
 
-	point = mlx_get_data_addr(img_ptr, &bits_per_pixel, &size_line, &endian);
+	point = (t_uint *)mlx_get_data_addr(img_ptr, &bits_per_pixel, &size_line, &endian);
 
 	ft_printf("bits_per_pixel %d\n", bits_per_pixel);
 	ft_printf("size_line %d\n", size_line);
@@ -37,11 +37,11 @@ void	ft_load_image(char *filename)
 	ft_printf("point %p\n", point);
 	ft_printf("value %d\n", *point);
 
-	for(int y = 0; y <  100; y++)
-		for(int x = 0; x < 1200; x++)
+	for(int y = 100; y <  204; y++)
+		for(int x = 100; x < 220; x++)
 		{
 			//ft_printf("value %d\n", point[ (y * 64) + x]);
-			mlx_pixel_put(game->gl.mlx_ptr, game->gl.win_ptr, x, y, point[ ((y * 4) * 1200) + (x * 4)]);
+			mlx_pixel_put(game->gl.mlx_ptr, game->gl.win_ptr, x-100, y -100, ((unsigned int)point[((y ) * 1200) + (x )]));
 
 		}
 
--- so_long/src/utils.h
+++ so_long/src/utils.h
@@ -6,13 +6,13 @@
 /*   By: dthalman <daniel@thalmann.li>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 23:23:55 by dthalman          #+#    #+#             */
-/*   Updated: 2021/11/12 15:00:57 by dthalman         ###   ########.fr       */
+/*   Updated: 2021/11/12 15:11:08 by dthalman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
-# define SWAP_INT32(x) (((x) >> 24) | (((x) & 0x00FF0000) >> 8) | (((x) & 0x0000FF00) << 8) | ((x) << 24)) 
+# define SWAP_INT32(x) (((x & 0xFF000000) >> 24) | (((x) & 0x00FF0000) >> 8) | (((x) & 0x0000FF00) << 8) | ((x & 0x000000FF) << 24)) 
 
 typedef unsigned int	t_uint;
 typedef struct s_position
