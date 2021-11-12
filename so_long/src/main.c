82c897c82a2ff23d808d346b3438c81b7a8808df
Daniel Thalmann <daniel@thalmann.li>
2021-11-12 02:23:12 -0800

    so_long

--- so_long/src/main.c
+++ so_long/src/main.c
@@ -6,13 +6,24 @@
 /*   By: dthalman <daniel@thalmann.li>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 23:36:40 by dthalman          #+#    #+#             */
-/*   Updated: 2021/11/11 23:51:37 by dthalman         ###   ########.fr       */
+/*   Updated: 2021/11/12 02:17:38 by dthalman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "mlx.h"
+
 int main(int argc, char **argv)
 {
+	void	*mlx_ptr;
+	void	*win_ptr;
+
+
 	(void)argc;
 	(void)argv;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "so_long");
+	mlx_loop(mlx_ptr);
+	
 	return (0);
 }
