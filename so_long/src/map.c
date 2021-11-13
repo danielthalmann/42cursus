4ba3ef964182b84fc2e25137d7de7425c8b51f51
Daniel Thalmann <daniel@thalmann.li>
2021-11-13 15:11:36 +0100

    so_long

--- so_long/src/map.c
+++ so_long/src/map.c
@@ -6,13 +6,73 @@
 /*   By: dthalman <daniel@thalmann.li>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 23:21:01 by dthalman          #+#    #+#             */
-/*   Updated: 2021/11/12 14:19:11 by dthalman         ###   ########.fr       */
+/*   Updated: 2021/11/13 15:11:08 by dthalman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map.h"
 
-int ft_load_map(char *filename)
+int ft_load_map(char *filename, t_map *map)
 {
+	int		fd;
+	char	*l;
+	t_list	*lines;
 
+	fd = open(filename, O_RDONLY);
+	ft_printf("%s\n", filename);	
+	if(fd == -1)
+		return (0);
+		//ft_printf("Impossible d'ouvrir le fichier (%s).\n", filename);
+	else
+	{
+		lines = 0;
+		l = get_next_line(fd);
+		while (l)
+		{
+			ft_lstadd_back(&lines, ft_lstnew(l));
+			l = get_next_line(fd);
+		}
+	}
+	close(fd);
+	ft_Load_list_map(lines, map);
+	ft_free_list_map(lines);
+	return (1);
+}
+
+int	ft_Load_list_map(t_list *list, t_map *map)
+{
+	int	x;
+	int y;
+
+	if (!list)
+		return (0);
+	map->size.height = ft_lstsize(list);
+	map->size.width = ft_strlen(list->content);
+	map->coord = malloc(sizeof(t_uint) * map->size.height * map->size.width);
+	while (y < map->size.height)
+	{
+		while (x < map->size.width)
+		{
+			map->coord= malloc(sizeof(t_uint) * map->size.height * map->size.width);
+			
+		}
+	}
+}
+
+/**
+ * @brief libère de la mémoire toute la liste
+ * 
+ * @param list 
+ */
+void	ft_free_list_map(t_list *list)
+{
+	t_list	*to_free;
+
+	while (list)
+	{
+		free(list->content);
+		to_free = list;
+		list = list->next;
+		free(to_free);
+	}
 }
--- so_long/src/map.h
+++ so_long/src/map.h
@@ -6,18 +6,22 @@
 /*   By: dthalman <daniel@thalmann.li>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 23:21:01 by dthalman          #+#    #+#             */
-/*   Updated: 2021/11/12 14:28:12 by dthalman         ###   ########.fr       */
+/*   Updated: 2021/11/13 15:10:13 by dthalman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAP_H
 # define MAP_H
 # include "utils.h"
+# include "game.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
 typedef struct s_map
 {
 	t_size	size;
-	t_uint	**coord;
+	t_uint	*coord;
 }	t_map;
 
-int ft_load_map(char *filename);
+int ft_load_map(char *filename, t_map *map);
 #endif
