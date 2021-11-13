743401f6cd3e7493f09aa355111235d04975404b
Daniel Thalmann <daniel@thalmann.li>
2021-11-13 15:23:13 +0100

    so_long

--- so_long/src/player.h
+++ so_long/src/player.h
@@ -6,15 +6,17 @@
 /*   By: dthalman <daniel@thalmann.li>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 23:17:47 by dthalman          #+#    #+#             */
-/*   Updated: 2021/11/11 23:51:50 by dthalman         ###   ########.fr       */
+/*   Updated: 2021/11/13 15:23:12 by dthalman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PLAYER_H
 # define PLAYER_H
 # include "utils.h"
+
 typedef struct s_player
 {
+	int	state;
 	t_position position;
 }	t_player;
 #endif
