3137a569ddfd98e034c77e9ec3aa1b02d328f2a1
Daniel Thalmann <dthalman@c1r5s8.42lausanne.ch>
2021-11-14 17:30:01 +0100

    so_long

--- so_long/src/sprite.c
+++ so_long/src/sprite.c
@@ -6,7 +6,7 @@
 /*   By: dthalman <daniel@thalmann.li>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 06:39:26 by dthalman          #+#    #+#             */
-/*   Updated: 2021/11/14 12:14:24 by dthalman         ###   ########.fr       */
+/*   Updated: 2021/11/14 16:16:40 by dthalman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,28 @@ void	ft_load_image(char *filename)
 	game = ft_game();
 	img_ptr = mlx_png_file_to_image(game->gl.mlx_ptr, filename, (int *)&(size.width), (int *)&(size.height));
 
+	t_position src;
+	t_position dest;
+	t_position isize;
 	
+	src.x = 100;
+	src.y = 100;
+	
+	dest.x = 0;
+	dest.y = 0;
+
+	isize.x = 200;
+	isize.y = 200;
+
+	ft_draw_image(img_ptr, &(game->gl), src, dest, isize);
+
+/*	
 	t_uint *point = 0;
 	int bits_per_pixel = 0;
 	int size_line = 0;
 	int endian = 0;
 
 	point = (t_uint *)mlx_get_data_addr(img_ptr, &bits_per_pixel, &size_line, &endian);
-
-	ft_printf("bits_per_pixel %d\n", bits_per_pixel);
-	ft_printf("size_line %d\n", size_line);
-	ft_printf("endian %d\n", endian);
-	ft_printf("point %p\n", point);
-	ft_printf("value %d\n", *point);
 
 	for(int y = 100; y <  204; y++)
 		for(int x = 100; x < 220; x++)
@@ -45,11 +54,11 @@ void	ft_load_image(char *filename)
 			mlx_pixel_put(game->gl.mlx_ptr, game->gl.win_ptr, x-100, y -100, ((unsigned int)point[((y ) * 1200) + (x )]));
 
 		}
-
+*/
 	//mlx_put_image_to_window  (game->gl.mlx_ptr, game->gl.win_ptr, img_ptr, 2, 2);
 }
 
-void	ft_draw_image(void *img_ptr, t_gl *gl, t_position src, t_position dest)
+void	ft_draw_image(void *img_ptr, t_gl *gl, t_position src, t_position dest, t_position size)
 {
 	char		*ptr;
 	int			bits_per_pixel = 0;
@@ -57,13 +66,25 @@ void	ft_draw_image(void *img_ptr, t_gl *gl, t_position src, t_position dest)
 	int			endian = 0;
 	t_position	c;
 
+	c.x = 0;
+	c.y = 0;
 	ptr = mlx_get_data_addr(img_ptr, &bits_per_pixel, &size_line, &endian);
-	for(int y = 100; y <  204; y++)
-		for(int x = 100; x < 220; x++)
-		{
-			//ft_printf("value %d\n", point[ (y * 64) + x]);
-			mlx_pixel_put(gl->mlx_ptr, gl->win_ptr, x-100, y -100, ((unsigned int*)ptr)[((y ) * 1200) + (x )]);
+	
+	ft_printf("bits_per_pixel %d\n", bits_per_pixel);
+	ft_printf("size_line %d\n", size_line);
+	ft_printf("endian %d\n", endian);
+	ft_printf("point %p\n", ptr);
+	ft_printf("value %d\n", *ptr);
 
-		}	
+	while(dest.y + c.y < dest.y + size.y)
+	{
+		while(dest.x + c.x < dest.x + size.x)
+		{
+			mlx_pixel_put(gl->mlx_ptr, gl->win_ptr, dest.x, dest.y, ((unsigned int*)ptr)[((src.y + c.y) * (size_line / (bits_per_pixel / 8))) + (src.x + c.x)]);
+			c.x++;
+		}
+		c.y++;
+	}
+	
 
 }
--- so_long/src/sprite.h
+++ so_long/src/sprite.h
@@ -6,7 +6,7 @@
 /*   By: dthalman <daniel@thalmann.li>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 06:39:26 by dthalman          #+#    #+#             */
-/*   Updated: 2021/11/12 14:27:30 by dthalman         ###   ########.fr       */
+/*   Updated: 2021/11/14 16:11:15 by dthalman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,5 @@
 # include "game.h"
 
 void	ft_load_image(char *filename);
+void	ft_draw_image(void *img_ptr, t_gl *gl, t_position src, t_position dest, t_position size);
 #endif
