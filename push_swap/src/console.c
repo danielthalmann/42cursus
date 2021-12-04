226632ce07ba2d8351573ab47af65ca3d44370b8
Daniel Thalmann <daniel@thalmann.li>
2021-12-05 00:52:34 +0100

    push_swap

---
+++ push_swap/src/console.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <graphics.h>
+
+#define clear() printf("\033[H\033[J")
+#define gotoxy(x,y) printf("\033[%d;%dH", (y), (x))
+
+
+int main (int argc, char **argv)
+{
+	int number;
+
+    clear();
+    printf(
+        "Enter your number in the box below\n"
+        "+-----------------+\n"
+        "|                 |\n"
+        "+-----------------+\n"
+    );
+    gotoxy(2, 3);
+    scanf("%d", &number);
+
+	return (0);
+}
