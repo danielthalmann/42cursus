e7a60ed5f91eef8656c3d7a1851ded4a708ef779
Daniel Thalmann <daniel@thalmann.li>
2021-12-13 20:31:39 +0100

    examshell

--- examshell/getnextline/gnl.c
+++ examshell/getnextline/gnl.c
@@ -6,17 +6,21 @@ char *gnl(int fd)
 {
 	static char	buf[BUFFER_SIZE + 1];
 	int len;
-	
+	char *ret;
+
+	ret = 0;
 	len = 1;
 	if (!*buf)
 	{
 		len = read(fd, buf, BUFFER_SIZE);
+		buf[len] = 0;
+		len = ft_lentoend(buf);
 	}
-	if (len < 1) 
+	while (len > 0) 
 	{
-		return (0);
+		malloc
 	}
-	
+	return (ret);
 }
 
 int	ft_lentoend(char *s)
