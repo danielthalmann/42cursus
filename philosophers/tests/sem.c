8c601384e7cd6612363d41660df30f0c00c3072a
Daniel Thalmann <dthalman@c1r5s8.42lausanne.ch>
2022-02-02 13:50:32 +0100

    philosophers

--- philosophers/tests/sem.c
+++ philosophers/tests/sem.c
@@ -1,7 +1,23 @@
 
 #include <semaphore.h>
+#include <unistd.h>
+#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
+	sem_t *s;
+	int	n = 3;
 
+	s = sem_open("./test.sem", O_CREAT);
+	for(int i=0; i<n; i++)
+	{
+  	    printf("[%d] fork\n", getpid());
+  		fork();
+		sem_wait(s);
+	}
+
+	printf("[%d] say hello\n", getpid());
+	//s = sem_open("./test.sem", O_CREAT);
+	//sem_wait(s);
+	// sem_close(s);
 }
