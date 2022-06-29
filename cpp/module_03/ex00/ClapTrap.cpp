4b4d3634592e50969586f0e0530ccb9b45d82785
Daniel Thalmann <daniel@thalmann.li>
2022-06-29 08:46:37 +0200

    module 03

--- cpp/module_03/ex00/ClapTrap.cpp
+++ cpp/module_03/ex00/ClapTrap.cpp
@@ -1,15 +1,28 @@
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap() :
-_name("ClapTrpap"),
-_hit(10),
-_energy(10),
-_damage(0)
+	_name	("ClapTrpap"),
+	_hit	(10),
+	_energy	(10),
+	_damage	(0)
 {
 
 }
 
-ClapTrap::ClapTrap(const ClapTrap &fixed) :
+ClapTrap::ClapTrap(const std::string name, const int hit, const int energy, const int damage) :
+	_name	(name),
+	_hit	(hit),
+	_energy	(energy),
+	_damage	(damage)
+{
+
+}
+
+ClapTrap::ClapTrap(const ClapTrap &claptrap) :
+	_name	(claptrap._name),
+	_hit	(claptrap._hit),	
+	_energy	(claptrap._energy),
+	_damage	(claptrap._damage)
 {
 	std::cout << "Copy constructor called" << std::endl;
 }
@@ -19,7 +32,7 @@ ClapTrap::~ClapTrap()
 	std::cout << "Destructor called" << std::endl;
 }
 
-ClapTrap & ClapTrap::operator=(const ClapTrap &clapTrap)
+ClapTrap &ClapTrap::operator=(const ClapTrap &clapTrap)
 {
 	_name = clapTrap._name;
 	_hit = clapTrap._hit;
@@ -29,6 +42,39 @@ ClapTrap & ClapTrap::operator=(const ClapTrap &clapTrap)
 	return *this;
 }
 
-	void attack(const std::string& target);
-	void takeDamage(unsigned int amount);
-	void beRepaired(unsigned int amount);
+void ClapTrap::attack(const std::string& target)
+{
+	if (_hit > 0)
+	{
+		if (_energy > 0)
+		{
+			std::cout << _name << " attacks " << target << ", causing " << _damage << " point of domage!" << std::endl;
+			_energy--;
+		}
+		else
+			std::cout << _name << " can't attack, no more energy" << std::endl;
+
+	}
+	else
+		std::cout << _name << " can't attack, is dead" << std::endl;
+
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	_hit -= amount;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (_energy > 0)
+	{
+		std::cout << _name << " repaire, " << amount << " point of live!" << std::endl;
+		_hit += amount;
+		_energy--;
+	}
+	else
+		std::cout << _name << " can't repaire, no more energy" << std::endl;
+
+}
+
--- cpp/module_03/ex00/main.cpp
+++ cpp/module_03/ex00/main.cpp
@@ -1,34 +1,15 @@
 
 #include <iostream>
-#include "Fixed.hpp"
+#include "ClapTrap.hpp"
 
 
 void testA( void )
 {
-	Fixed       a;
-	Fixed const b( 10 );
-	Fixed const c( 42.42f );
-	Fixed const d( b );
-	
-	a = Fixed( 1234.4321f );
 
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 }
 
 int main ( void )
 {
 	
-	std::cout << std::endl << std::endl;
- 	testA();
-	std::cout << std::endl << std::endl;
-
 	return (0);
 }
