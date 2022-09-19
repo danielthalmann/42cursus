20ca38b8e04e0e6228a8c42881d7ef0c1d046f30
Daniel Thalmann <daniel@thalmann.li>
2022-09-19 09:49:52 +0200

    Module07 ex00

--- cpp/module_07/ex00/main.cpp
+++ cpp/module_07/ex00/main.cpp
@@ -9,9 +9,30 @@ void print (int &a, int &b)
 	std::cout << std::endl;
 }
 
-int main ( void )
+void testA(void)
 {
+	int a = 2;
+	int b = 3;
+	
+	::swap( a, b );
 
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+}
+
+void testB(void)
+{
 	int a = 4;
 	int b = 2;
 	
@@ -42,8 +63,11 @@ int main ( void )
 		std::cout << std::endl;
 	}
 
+}
 
+int main ( void )
+{
 
-
+	testA();
 	return (0);
 }
