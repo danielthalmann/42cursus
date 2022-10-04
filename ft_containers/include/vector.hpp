8f0d017f9aa01b6776cca4e8ff8af54391440958
Daniel Thalmann <daniel@thalmann.li>
2022-10-04 20:54:49 +0200

    ft_containers

--- ft_containers/include/vector.hpp
+++ ft_containers/include/vector.hpp
@@ -2,12 +2,33 @@
 #define VECTOR_HPP
 
 #include <iostream>
-#include <vector>
+#include <memory>
 
-template <class T, class container = allocator<T> >
+template <class T, class alloc = std::allocator<T> >
 class vector
 {
 public:
+      typedef T						value_type;
+      typedef T*						pointer;
+      typedef const T*					const_pointer;
+      typedef T&						reference;
+      typedef const T&					const_reference;
+      typedef std::iterator<std::input_iterator_tag, 
+            value_type, 
+            value_type, 
+            pointer, 
+            reference> iterator;
+      typedef     const std::iterator<std::input_iterator_tag, 
+            value_type, 
+            value_type, 
+            pointer, 
+            reference> const_iterator;
+      typedef std::reverse_iterator<const_iterator>	const_reverse_iterator;
+      typedef std::reverse_iterator<iterator>		reverse_iterator;
+      typedef size_t					size_type;
+      typedef ptrdiff_t					difference_type;
+      typedef _Alloc					allocator_type;
+
 	vector<T>() {
 
 	}
--- ft_containers/tests/main.cpp
+++ ft_containers/tests/main.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "allocator.hpp"
 
-int
-main(int argc, char* argv[])
+
+int main(int argc, char* argv[])
 {
     allocator<int> i;
-    
-    
+    (void) i;
+    (void) argc;
+    (void) argv;
+
     printf("Hello world\n");
     return 0;
 }
