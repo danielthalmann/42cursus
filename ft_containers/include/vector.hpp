4b141cb6beca7760beb3bbbbacf2b118a9642ec0
Daniel Thalmann <dthalman@c1r5s8.42lausanne.ch>
2022-10-12 14:25:54 +0200

    ft_containter

--- ft_containers/include/vector.hpp
+++ ft_containers/include/vector.hpp
@@ -36,51 +36,109 @@ namespace ft
 		 * 
 		 */
 		vector () {
-
+			// TODO
 		}
 		vector(size_type n, const_reference value, allocator_type a = allocator_type()) : _allocator(a), _size(n)
 		{ 
+			// TODO
 			(void) value;
 			allocate (n);
 		}
-		vector(vector &vector);
-		virtual ~vector();
-		vector &operator=(const vector &vector);
 
-		iterator begin() 				{	return iterator(this->_start); }
-		const_iterator begin() const	{	return const_iterator(this->_start); }
-		iterator end()					{	return iterator(this->_end); }
-		const_iterator end() const		{	return const_iterator(this->_end); }
+		vector(vector &vector) {	
+			// TODO 
+		}
+		
+		virtual ~vector() {
+			// TODO
+		}
+		
+		vector &operator=(const vector &vector) {
+			// TODO
+		}
 
-		size_type size()				{ 	return size_type(this->_finish - this->_start); }
+		void assign(size_type n, const_reference v) {
+			// TODO
+		}
 
-		size_type capacity() const		{	return size_type(this->_end - this->_start); }
+		template< class InputIt >
+		void assign( InputIt first, InputIt last ) {
+			// TODO
+		}
 
-		size_type max_size() const		{	return size_type(_allocator.max_size()); }
-    	
+		allocator_type get_allocator() const	{	return this->_allocator; };
+
+
+		/**
+		 * Element access 
+		 */
+
+    	reference at(size_type n) 				{	range_check(n); return (*this)[n]; }
+    	const_reference at(size_type n) const	{	range_check(n); return (*this)[n]; }
+		reference operator[](size_type n)		{	return *(this->_start + n); }
+		const_reference operator[](size_type n) const	{	return *(this->_start + n); }
+		reference front()						{	return *begin(); }
+		const_reference front()	const			{	return *begin(); }
+		reference back()						{	return *(end() - 1); }
+		const_reference back()	const			{	return *(end() - 1); }
+		pointer data()							{	return this->_start; }
+		const_pointer data()	const			{	return this->_start; }
+
+
+		/**
+		 * Iterators
+		 */
+
+		iterator begin() 						{	return iterator(this->_start); }
+		const_iterator begin() const			{	return const_iterator(this->_start); }
+		iterator end()							{	return iterator(this->_end); }
+		const_iterator end() const				{	return const_iterator(this->_end); }
+    
+		reverse_iterator       rbegin()			{	return reverse_iterator(this->_end); }
+		const_reverse_iterator rbegin()  const	{	return const_reverse_iterator(this->_end); }
+		reverse_iterator       rend()			{	return reverse_iterator(this->_start); }
+		const_reverse_iterator rend() const 	{	return const_reverse_iterator(this->_start); }
+
+		/**
+		 * Capacity
+		 */
+
+		bool empty()							{	return begin() == end(); }
+		size_type size()						{ 	return size_type(this->_finish - this->_start); }
+		size_type max_size() const				{	return size_type(_allocator.max_size()); }
+		void reserve( size_type n )		{	
+		    if (n > capacity())
+			{
+				pointer oldstart = this->_start;
+				pointer oldend = this->_end;
+				size_type s = size();
+				this->_start = _allocator.allocate(n, this->_start);
+				for (size_type i = 0; i < s; ++i)
+					_allocator.construct(this->_start + i, *(oldstart + i));
+				this->_end = this->_start + n;
+				this->_finish = this->_start + s;
+				_allocator.dealocate(oldstart, oldend - oldstart);
+			}
+		}
+		size_type capacity() const				{	return size_type(this->_end - this->_start); }
+
+
+		/**
+		 *  Modifiers 
+		 */
+		void clear()							{	this->_finish = this->_start; }
+
+		iterator insert( const_iterator pos, const T& value ) {
+
+		}
+
+
 		void resize(size_type n, value_type v = value_type()) {
 			if (n > size())
 				fill_insert(this->_start, n - size(), v);
 			else if (n < size())
 				erase_at_end(this->_start + n);
 		}
-
-		bool empty()							{	return begin() == end(); }
-
-		reference operator[](size_type n)		{	return *(this->_start + n); }
-		const_reference operator[](size_type n) const	{	return *(this->_start + n); }
-
-    	reference at(size_type n) 				{	range_check(n); return (*this)[n]; }
-    	const_reference at(size_type n) const	{	range_check(n); return (*this)[n]; }
-
-		reference front()						{	return *begin(); }
-		const_reference front()	const			{	return *begin(); }
-
-		reference back()						{	return *(end() - 1); }
-		const_reference back()	const			{	return *(end() - 1); }
-
-		pointer data()							{	return this->_start; }
-		const_pointer data()	const			{	return this->_start; }
 
 	    void push_back(const value_type& v) {
 
@@ -94,20 +152,22 @@ namespace ft
 
 		}
 
-
 	protected:
 
 		void assign( size_type count, const T& value );
 		void allocate(size_type n) { _start = _allocator.allocate(n); this->_finish = this->_start; _end = this->_start + n; }
+		
 		void fill_insert(iterator position, size_type n, reference v) {	
 			for (; __first != __last; ++__first, (void)++__cur)
 				_allocator.construct(__alloc, std::__addressof(*__cur), *__first);
 
 		}
+
 		void erase_at_end(pointer end) {	
-			_allocator.deallocate(this->_end - end);
+			_allocator.deallocate(end, this->_end - end);
 			this->_end = end;
 		}
+
 		void range_check(size_type n) const
 		{
 			if (n >= this->size())
@@ -119,7 +179,6 @@ namespace ft
 		pointer			_start;
 		pointer			_end;
 		pointer			_finish;
-		size_type		_size;
 		allocator_type	_allocator;
 
 
