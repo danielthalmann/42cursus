fddcca60d475138b85d162a53cdd2e4f7b94982b
Daniel Thalmann <daniel@thalmann.li>
2022-12-09 14:06:22 +0100

    test enable_if

---
+++ ft_containers/tests/test_enable.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <type_traits>
+
+
+template<typename>
+struct is_json_type_helper
+: public std::false_type 
+{ };
+
+template<>
+struct is_json_type_helper<std::string>
+: public std::true_type
+{ };
+
+template<typename T>
+struct is_json_type
+: public is_json_type_helper< T >
+{ };
+
+
+template <typename T>
+int get(int, typename std::enable_if< is_json_type<T>::value, int>::type = 0 )
+{
+    std::cout << "string";
+    return 0;
+};
+
+
+template <typename T>
+int get(int, typename std::enable_if< !is_json_type<T>::value, int>::type = 0 )
+{
+    std::cout << "unknow";
+    return 0;
+};
+
+
+void  test ( )
+{
+
+    get<std::string>(0);
+    get<int>(0);
+
+}
+
+int main( void ) 
+{
+	test();
+
+	return (0);
+}
